5fdb906814f1b1bcf1072c2333d92587321f3bc4
Vitaliy <networkvetalius@gmail.com>
2021-12-03 16:00:12 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsListEl = document.querySelector('ul#ingredients');
+
+const ingredientsItemArr = ingredients.map(ingredient => {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.classList.add('item');
+  ingredientsEl.textContent = ingredient;
+  return ingredientsEl;
+})
+console.log(ingredientsItemArr);
+ingredientsListEl.append(...ingredientsItemArr);
+
+
+
