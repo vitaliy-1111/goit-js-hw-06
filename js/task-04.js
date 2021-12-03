af245af4c381a25c6c3f075f5ad5aadf20ff4ca1
Vitaliy <networkvetalius@gmail.com>
2021-12-03 21:56:48 +0200

    task-04 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,16 @@
-const incrementBtnEl = document.querySelector("[data-action]=decrement");
-console.log(incrementBtnEl);
+const incrementBtnEl = document.querySelector('[data-action="increment"]');
+const decrementBtnEl = document.querySelector('[data-action="decrement"]');
+const counterEl = document.querySelector('#value');
+let counterValue = 0;
+
+incrementBtnEl.addEventListener('click', onAddIncrement);
+decrementBtnEl.addEventListener('click', onAddDecrement);
+
+function onAddIncrement() {
+  counterValue += 1;
+  return counterEl.textContent = counterValue;
+}
+function onAddDecrement() {
+  counterValue -= 1;
+  return counterEl.textContent = counterValue;
+}
