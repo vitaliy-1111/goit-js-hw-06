51d8c7e4400f4031e6311d88577f2bb99a64aa70
Vitaliy <networkvetalius@gmail.com>
2021-12-04 14:40:00 +0200

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,35 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All fields must be filled');
+    return;
+  }
+
+  const DataForm = {
+    email,
+    password
+  }
+  console.log(DataForm);
+  event.currentTarget.reset();
+
+}
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//  const formData = new FormData(event.currentTarget);
+  
+//   formData.forEach((value, name) => {
+//     if (value === '') {
+//       alert('All fields must be filled');
+//       return;
+//     }
+//     console.log( name, value);
+//   });
+// }
