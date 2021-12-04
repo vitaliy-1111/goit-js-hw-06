19e5efb116258c7740cfd5bab28b7f418487cef3
Vitaliy <networkvetalius@gmail.com>
2021-12-04 14:54:06 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColorEl = document.querySelector('button.change-color');
+const colorTextEl = document.querySelector('span.color');
+
+buttonChangeColorEl.addEventListener('click', onChangeBodyColor);
+
+function onChangeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorTextEl.textContent = getRandomHexColor();
+}
